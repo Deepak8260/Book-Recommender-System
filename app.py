b52911cb5f3dbf24f52e71a4eff059f24efc62d8
Archana Jena <jenaarchana412@gmail.com>
2024-12-10 20:00:32 +0530

    contact page added and the size of background image of home page is decreased

--- app.py
+++ app.py
@@ -45,5 +45,13 @@ def recommend():
 
 	return render_template('recommend.html',data=data)
 
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 if __name__ == '__main__':
 	app.run(debug=True)
